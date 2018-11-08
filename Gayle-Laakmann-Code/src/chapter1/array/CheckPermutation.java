24ad1e28f7362852f192294b794308c429ee37ef
KrishnakanthYachareni <yacharenikrishnakanth@gmail.com>
2018-11-08 23:26:31 +0530

    permutaion of a string question.

---
+++ Gayle-Laakmann-Code/src/chapter1/array/CheckPermutation.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018. All rights Reserved By Krishnakanth Yachareni
+ */
+
+package chapter1.array;
+
+import java.util.List;
+
+/**
+ * @author Krishnakanth Yachareni
+ * @since 08-11-2018
+ * <p>
+ * Given two strings, write a method to decide if one is a permutation of other.
+ * </p>
+ * Time complexity :
+ * Space complexity :
+ */
+public class CheckPermutation {
+    public static void main(String[] args) {
+        boolean result = checkPermutation("abcd", "dcba"); // true
+        System.out.println(result);
+    }
+
+    private static boolean checkPermutation(String str1, String str2) {
+
+        return true;
+    }
+
+    private static void permutations(String str, List<String> list) {
+        
+    }
+}
