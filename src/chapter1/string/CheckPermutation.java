d5f371cf518a9d0e843fcde299df9b0ce4e58f34
KrishnakanthYachareni <yacharenikrishnakanth@gmail.com>
2018-11-10 16:22:29 +0530

    check permutation implemented

--- src/chapter1/string/CheckPermutation.java
+++ src/chapter1/string/CheckPermutation.java
@@ -4,29 +4,61 @@
 
 package chapter1.string;
 
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * @author Krishnakanth Yachareni
- * @since 08-11-2018
+ * @since 10-11-2018
  * <p>
  * Given two strings, write a method to decide if one is a permutation of other.
  * </p>
- * Time complexity :
- * Space complexity :
+ * Time complexity : O(n+m) where n = one string length, m = other string length.
+ * Space complexity : O(n+m)
  */
 public class CheckPermutation {
     public static void main(String[] args) {
-        boolean result = checkPermutation("abcd", "dcba"); // true
-        System.out.println(result);
+        boolean resultA = checkPermuteMethodA("abcd", "dcba"); // true
+        boolean resultB = checkPermuteMethodB("wabd", "dcbaw"); // false
+        System.out.println(resultA);
+        System.out.println(resultB);
     }
 
-    private static boolean checkPermutation(String str1, String str2) {
-
+    /**
+     * Implementation based on ASCII 128 characters.
+     * Time Complexity is not good for this solution.
+     */
+    public static boolean checkPermuteMethodA(String source, String destination) {
+        if (source.length() != destination.length()) {
+            return false;
+        }
+        int[] letters = new int[128];
+        char[] strArray = source.toCharArray();
+        for (char ch : strArray) {
+            letters[ch]++;
+        }
+        for (int i = 0; i < destination.length(); i++) {
+            int c = destination.charAt(i);
+            letters[c]--;
+            if (letters[c] > 0) {
+                return false;
+            }
+        }
         return true;
     }
 
-    private static void permutations(String str, List<String> list) {
+    /**
+     * Implementation based on sorting of a string.
+     */
+    public static boolean checkPermuteMethodB(String source, String destination) {
+        if (source.length() != destination.length()) {
+            return false;
+        }
+        return Arrays.equals(sort(source), sort(destination));
+    }
 
+    private static char[] sort(String str) {
+        char[] content = str.toCharArray();
+        Arrays.sort(content);
+        return content;
     }
 }
