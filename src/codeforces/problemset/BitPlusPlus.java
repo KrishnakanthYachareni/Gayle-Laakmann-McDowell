011af033fa7594558379ee84ffe7cc37b1289cda
KrishnakanthYachareni <yacharenikrishnakanth@gmail.com>
2020-06-11 21:50:17 +0530

    Codeforce Problems A solved

--- src/codeforces/problemset/BitPlusPlus.java
+++ src/codeforces/problemset/BitPlusPlus.java
@@ -10,6 +10,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+/**
+ * @link https://codeforces.com/problemset/problem/282/A
+ */
 public class BitPlusPlus {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
