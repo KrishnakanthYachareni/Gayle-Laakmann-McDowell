62abcd21982918425fd1e4c8d8cfb1bc8b46086a
yacharenikrishnakanth <yacharenikrishnakanth@gmail.com>
2021-11-29 17:30:04 -0600

    Trapping rain water leetcode

--- src/leetcode/facebook/arrays/TrappingRainingWater.java
+++ src/leetcode/facebook/arrays/TrappingRainingWater.java
@@ -7,6 +7,40 @@ public class TrappingRainingWater {
 
 
     /**
+     * If there is a larger bar at one end (say right), we are assured that the water trapped would be
+     * dependent on height of bar in current direction (from left to right). As soon as we find the bar at other end (right) is smaller,
+     * we start iterating in opposite direction (from right to left).
+     * <p>
+     * Explanation: Max Trapping water is always depends on the minimum height of the bar Hence
+     * <p>
+     * Approach 4
+     * https://leetcode.com/problems/trapping-rain-water/solution/
+     */
+    public int trap(int[] height) {
+        int n = height.length;
+        int maxWater = 0, maxLeft = 0, maxRight = height[n - 1];
+        int p1 = 0, p2 = n - 1;
+        while (p1 < p2) {
+            if (height[p1] < height[p2]) {
+                int currWater = maxLeft - height[p1];
+                if (currWater >= 0) {
+                    maxWater += currWater;
+                }
+                maxLeft = Math.max(maxLeft, height[p1]);
+                p1++;
+            } else {
+                int currWater = maxRight - height[p2];
+                if (currWater >= 0) {
+                    maxWater += currWater;
+                }
+                maxRight = Math.max(maxRight, height[p2]);
+                p2--;
+            }
+        }
+        return maxWater;
+    }
+
+    /**
      * Brute Force, time = O(n^2), space = O(1)
      * At every position in an array
      *
