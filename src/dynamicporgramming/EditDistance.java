5f6895706fa161f71c86030ab0eb1108191de5c9
yacharenikrishnakanth <yacharenikrishnakanth@gmail.com>
2022-04-21 23:18:24 -0500

    Added the levenshteinDistance

--- src/dynamicporgramming/EditDistance.java
+++ src/dynamicporgramming/EditDistance.java
@@ -16,11 +16,15 @@ public class EditDistance {
     public static int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
-        // First Row: each cell represents the number of characters so far word1
+
+        /* First Row: each cell represents
+        the number of characters so far in w1
+         */
         for (int j = 1; j <= n; j++)
             dp[0][j] = j;
 
-        // Second Column: each cell represents the number of characters so far word2
+        /* Second Column: each cell represents
+        the number of characters so far w2 */
         for (int i = 1; i <= m; i++)
             dp[i][0] = i;
 
@@ -29,7 +33,8 @@ public class EditDistance {
                 if (word1.charAt(i - 1) == word2.charAt(j - 1))
                     dp[i][j] = dp[i - 1][j - 1];
                 else
-                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                    dp[i][j] = 1 + Math.min(
+                            dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
             }
         return dp[m][n];
     }
@@ -40,17 +45,17 @@ public class EditDistance {
      * <p>
      * TC = O(3^n+m)
      */
-    public int minDistanceRec(String s1, String s2, int i, int j) {
-        int m = s1.length(), n = s2.length();
-        if (i == m) // if len of s1 > len of s2 (delete remaining chars in s1)
+    public int editDistance(String w1, String w2, int i, int j) {
+        int m = w1.length(), n = w2.length();
+        if (i == m) // if len of w1 > len of w2 (delete remaining chars in w1)
             return n - j;
-        else if (j == n) // if len of s2 > len of s1 (insert remaining chars in s2)
+        else if (j == n) // if len of w2 > len of w1 (insert remaining chars in w2)
             return m - i;
-        else if (s1.charAt(i) == s2.charAt(j))
-            return minDistanceRec(s1, s2, i + 1, j + 1);
+        else if (w1.charAt(i) == w2.charAt(j))
+            return editDistance(w1, w2, i + 1, j + 1);
         else
-            return 1 + Math.min(minDistanceRec(s1, s2, i + 1, j), // delete char
-                    Math.min(minDistanceRec(s1, s2, i, j + 1), // insert chat
-                            minDistanceRec(s1, s2, i + 1, j + 1))); // substitute
+            return 1 + Math.min(editDistance(w1, w2, i + 1, j), // delete char
+                    Math.min(editDistance(w1, w2, i, j + 1), // insert char
+                            editDistance(w1, w2, i + 1, j + 1))); // substitute
     }
 }
---
+++ src/dynamicporgramming/LevenshteinDistance.java
@@ -0,0 +1,63 @@
+package dynamicporgramming;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class LevenshteinDistance {
+
+    public static void main(String[] args) {
+//        String s1 = "intention", s2 = "execution";
+        System.out.println("Enter two strings");
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        int[][] decisionTable = minDistance(s1, s2);
+        System.out.println("Minimum Edit distance is: " + decisionTable[s1.length()][s2.length()]);
+        System.out.println("Operations are: ");
+        printChanges(s1, s2, decisionTable);
+    }
+
+    /**
+     * Bottom-up: Tabulation approach.
+     */
+    public static int[][] minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        // First Row: each cell represents the number of characters so far word1
+        for (int j = 1; j <= n; j++)
+            dp[0][j] = j;
+
+        // Second Column: each cell represents the number of characters so far word2
+        for (int i = 1; i <= m; i++)
+            dp[i][0] = i;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+            }
+        return dp;
+    }
+
+    public static void printChanges(String s1, String s2, int[][] decisionTable) {
+        int i = s1.length(), j = s2.length();
+        while (i != 0 && j != 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                i--;
+                j--;
+            } else if (decisionTable[i][j] == decisionTable[i - 1][j - 1] + 1) { // Replace the character
+                System.out.println("Substitute " + s1.charAt(i - 1) + " with " + s2.charAt(j - 1));
+                i--;
+                j--;
+            } else if (decisionTable[i][j] == decisionTable[i - 1][j] + 1) { // Delete the character
+                System.out.println("Delete " + s1.charAt(i - 1));
+                i--;
+            } else if (decisionTable[i][j] == decisionTable[i][j - 1] + 1) { // Insert the character
+                System.out.println("Insert " + s2.charAt(j - 1));
+                j--;
+            }
+        }
+    }
+}
