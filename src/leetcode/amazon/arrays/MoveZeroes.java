112e31ee3b8db13061b694bac145b60787cfbe47
yacharenikrishnakanth <yacharenikrishnakanth@gmail.com>
2022-05-26 21:45:36 -0500

    changes

---
+++ src/leetcode/amazon/arrays/MoveZeroes.java
@@ -0,0 +1,15 @@
+package leetcode.amazon.arrays;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int nonZero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0)
+                nums[nonZero++] = nums[i];
+        }
+
+        while (nonZero < nums.length) {
+            nums[nonZero++] = 0;
+        }
+    }
+}
